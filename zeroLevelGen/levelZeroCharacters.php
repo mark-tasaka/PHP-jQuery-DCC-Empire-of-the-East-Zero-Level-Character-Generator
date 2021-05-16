11ff67153bdeb4b13371be3924e435a71c2dfd8d
Mark Tasaka <mark.tasaka@gmail.com>
2021-05-16 15:02:39 -0700

    Update 1.02

--- zeroLevelGen/levelZeroCharacters.php
+++ zeroLevelGen/levelZeroCharacters.php
@@ -16,7 +16,7 @@
     
    <!--<script type="text/javascript" src="js/occupation.js"></script>-->
     
-    <script type="text/javascript" src="js/luckySign.js"></script>
+    <<!--script type="text/javascript" src="js/luckySign.js"></script>-->
    <script type="text/javascript" src="js/adjustments.js"></script>
     
     
@@ -34,6 +34,7 @@
     include 'php/message.php';
     include 'php/zeroLvOccupation.php';
     include 'php/abilityScoreGen.php';
+    include 'php/luckySign.php';
     
         
         if(isset($_POST["theSex"]))
@@ -1004,53 +1005,6 @@
   <script>
 	  
 
-      
-      	  /*
-	  rollDice (numberOfSides, numberOfDiceRolled, numberOfDiceRemoved, baseValue)
-	  
-	  numberOfSides - represents how many sides that they die has (ie. d4, d5, d6)
-	  
-	  numberOfDiceRolled - represents the number of dice rolled
-	  
-	  numberOfDiceRemoved - represents the number of dice removed (ie. roll 4 d6 and drop the lowest). The die/dice with the lowest values will always be removed.
-	  
-	  baseValue - represents the base value that it added to the die rolls.  For instance, a base value of 6 is added to the rolls of 2D6, which is represented as 2D6+6 in game mechanics.
-	  */
-	  	function rollDice(numberOfSides, numberOfDiceRolled, numberOfDiceRemoved, baseValue) {
-		
-		if(numberOfSides === undefined || typeof numberOfSides !== 'number'||numberOfSides <=2 || numberOfSides >=30){
-			numberOfSides = 6;
-		}
-		
-		if(numberOfDiceRolled < 0){
-			numberOfDiceRolled =2;
-		}
-		
-		if(numberOfDiceRemoved === undefined || typeof numberOfDiceRemoved !== 'number'||numberOfDiceRemoved >= numberOfDiceRolled){
-			numberOfDiceRemoved =0;
-		}
-		
-		if(baseValue === undefined || typeof baseValue !== 'number'||baseValue < 0){
-			baseValue = 0;
-		}
-		
-		let numbers = [];
-		
-
-		for(let i = 0; i < numberOfDiceRolled; i++){
-			numbers[i] = Math.floor((Math.random() * 6) + 1);
-		}
-		
-        numbers.sort();
-        
-		let sum = baseValue;
-			
-		for(let i = numberOfDiceRemoved; i < numberOfDiceRolled; i++){
-			sum += numbers[i];
-		}
-			
-		return sum ;
-	}
       
       
 	  
---
+++ zeroLevelGen/php/luckySign.php
@@ -0,0 +1,45 @@
+<?php
+
+
+function getLuckySign($input)
+{
+        $a00 = array("0", "Harsh winter", "All attack rolls");
+        $a01 = array("1", "The bull", "Melee attack rolls");
+        $a02 = array("2", "Fortunate date", "Missle fire attack rolls");
+        $a03 = array("3", "Raised by wolves", "Unarmed attack rolls");
+        $a04 = array("4", "Conceived on horseback", "Mounted attack rolls");
+        $a05 = array("5", "Born on the battlefield", "Damage rolls");
+        $a06 = array("6", "Path of the bear", "Melee damage rolls");
+        $a07 = array("7", "Hawkeye", "Missle damage rolls");
+        $a08 = array("8", "Pack hunter", "Attack & damage rolls for 0-level starting weapon");
+        $a09 = array("9", "Born under the loom", "Skill checks");
+        $a10 = array("10", "Fox's cunning", "Find/disable traps");
+        $a11 = array("11", "Four-leafed clover", "Find secret doors");
+        $a12 = array("12", "Seventh son", "Spell checks");
+        $a13 = array("13", "The raging storm", "Spell damage");
+        $a14 = array("14", "Righteous heart", "Turn unholy checks");
+        $a15 = array("15", "Surived the plague", "Magical healing");
+        $a16 = array("16", "Luck sign", "Saving throws");
+        $a17 = array("17", "Guardian angel", "Saving throws to escape traps");
+        $a18 = array("18", "Survived a spider bite", "Saving throws against poison");
+        $a19 = array("19", "Struck by Lightning", "Reflex saving throws");
+        $a20 = array("20", "Lived through famine", "Fortitude saving throws");
+        $a21 = array("21", "Resisted temptation", "Will saving throws");
+        $a22 = array("22", "Charmed house", "Armour class");
+        $a23 = array("23", "Speed of the Cobra", "Initiative");
+        $a24 = array("24", "Bountiful Harvest", "Hits points/each level");
+        $a25 = array("25", "Warrior's arm", "Critical hit tables");
+        $a26 = array("26", "Unholy house", "Corruption rolls");
+        $a27 = array("27", "The Broken Star", "Fumbles");
+        $a28 = array("28", "Birdsong", "Number of Languages");
+        $a29 = array("29", "Wild child", "Speed");
+        
+        $array1= array($a00, $a01, $a02, $a03, $a04, $a05, $a06, $a07, $a08, $a09, $a10, $a11, $a12, $a13, $a14, $a15, $a16, $a17, $a18, $a19, $a20, $a21, $a22, $a23, $a24, $a25, $a26, $a27, $a28, $a29);
+        
+        return $array1[$input];
+        
+}
+
+
+
+?>
